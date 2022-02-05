4b670d519ac3d9e84bc4961c21519e173237e152
Vivek Kaushik <kaushikvivek.17@gmail.com>
2022-02-05 16:02:14 +0000

    [Removed] .nuxt folder

--- .nuxt/components/index.js
+++
@@ -1,5 +0,0 @@
-export { default as Footer } from '../../components/Footer.vue'
-export { default as TopNavbar } from '../../components/TopNavbar.vue'
-
-export const LazyFooter = import('../../components/Footer.vue' /* webpackChunkName: "components/Footer" */).then(c => c.default || c)
-export const LazyTopNavbar = import('../../components/TopNavbar.vue' /* webpackChunkName: "components/TopNavbar" */).then(c => c.default || c)
